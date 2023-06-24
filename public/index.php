241530c1b3f3ff2a558e3d5cb2ad8a755308c05a
CarloAFlores <carlos2000sistemasc@gmail.com>
2023-06-25 00:13:58 +0600

    Se agrego las dependencias de bootstrap y sweetAlert

--- public/index.php
+++ public/index.php
@@ -3,11 +3,12 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 use Controllers\inicioController;
 
 $router = new Router();
 
-$router->get('/inico',[inicioController::class, 'index']);
+$router->get('/',[LoginController::class, 'index']);
 $router->get('/nosotros','funcion_nosotros');
 
 
--- views/layout.php
+++ views/layout.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App Salón</title>
+    <title>Sistema de Tickets</title>
+    <link rel="stylesheet" href="../../src/css/style.css">
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
 
 </head>
 <body>
+    <header>
+        <img src="" alt="logo_empresa">
+    </header>
 
     <?php echo $contenido; ?>
+
+    <footer>
+        <h1>FOOTER</h1>
+    </footer>
 </body>
 </html>
